fe1c4dcb9def688cbced8138a7b23c507ee7c959
Alain Chautard <achautard@gmail.com>
2018-05-06 16:52:55 -0700

    Added run total duration

--- src/app/run.ts
+++ src/app/run.ts
@@ -9,4 +9,13 @@ export abstract class Run {
   getSegments(): Array<Segment> {
     return this.segments;
   }
+
+  getTotalDuration(): string {
+    const duration = this.segments.reduce((acc, segment) => acc + segment.duration, 0);
+    return duration + ' '+ this.getUnit();
+  }
+
+  getUnit(): string {
+    return this.segments[0].unit;
+  }
 }
