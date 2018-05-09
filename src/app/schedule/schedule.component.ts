7f95e8af79c650e7a89945b8f4eb15e792ba292f
Alain Chautard <alain@interstate21.com>
2018-05-09 11:38:46 -0700

    Added schedule for current week

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FastFinishRun} from '../fast-finish-run';
+import {FoundationRun} from '../foundation-run';
+import {HillRepetitionRun} from '../hill-repetition-run';
+import {RecoveryRun} from '../recovery-run';
+import {LongRun} from '../long-run';
 
 @Component({
   selector: 'app-schedule',
@@ -6,6 +11,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./schedule.component.css']
 })
 export class ScheduleComponent implements OnInit {
+
+  monRun = new FastFinishRun(3);
+  tueRun = new FoundationRun(3);
+  wedRun = new FoundationRun(2);
+  thuRun = new HillRepetitionRun(2);
+  friRun = new RecoveryRun(3);
+  satRun = new LongRun(3);
 
   constructor() { }
 
