8c7125555e192cc428c3b46d5e6937d02382cb07
Alain Chautard <alain@interstate21.com>
2018-06-25 07:54:13 -0700

    Refactor all runs component + added a few missing run types

--- src/app/all-runs/all-runs.component.ts
+++ src/app/all-runs/all-runs.component.ts
@@ -1,14 +1,5 @@
 import { Component } from '@angular/core';
-import {FastFinishRun} from '../run-types/fast-finish-run';
-import {RecoveryRun} from '../run-types/recovery-run';
-import {HillRepetitionRun} from '../run-types/hill-repetition-run';
-import {LongRun} from '../run-types/long-run';
-import {FoundationRun} from '../run-types/foundation-run';
-import {TempoRun} from '../run-types/tempo-run';
-import {ShortIntervalRun} from '../run-types/short-interval-run';
-import {LongIntervalRun} from '../run-types/long-interval-run';
-import {LongRunSpeedPlay} from '../run-types/long-run-speed-play';
-import {ff, foun, getRuns, hill, lgint, long, longsp, rec, shint, temp} from '../run-types/run-factory';
+import {ff, foun, getRuns, hill, lgint, long, longff, longsp, mxint, rec, shint, temp} from '../run-types/run-factory';
 
 @Component({
   selector: 'app-all-runs',
@@ -17,14 +8,8 @@ import {ff, foun, getRuns, hill, lgint, long, longsp, rec, shint, temp} from '..
 })
 export class AllRunsComponent {
 
-  recoveryRuns: Array<RecoveryRun> = getRuns(9, rec);
-  foundationRuns: Array<FoundationRun> = getRuns(9, foun);
-  longRuns: Array<LongRun> = getRuns(9, long);
-  fastFinishRuns: Array<FastFinishRun> = getRuns(9, ff);
-  hillRepsRuns: Array<HillRepetitionRun> = getRuns(12, hill);
-  tempoRuns: Array<TempoRun> = getRuns(9, temp);
-  shortIntRuns: Array<ShortIntervalRun> = getRuns(9, shint);
-  longIntRuns: Array<LongIntervalRun> = getRuns(10, lgint);
-  longRunsSpeedPlay: Array<LongRunSpeedPlay> = getRuns(6, longsp);
-  
+  allRuns = [getRuns(9, rec), getRuns(9, foun), getRuns(9, long),
+              getRuns(9, ff), getRuns(12, hill), getRuns(9, temp),
+              getRuns(9, shint), getRuns(10, lgint), getRuns(6, longsp),
+              getRuns(6, longff), getRuns(4, mxint)];
 }
